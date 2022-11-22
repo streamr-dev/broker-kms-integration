220cc4d20b38332de4c4a176782ea8a43df2c4cc
Henri Pihkala <henri@streamr.network>
2022-11-22 14:53:53 +0100

    minimal stub

--- README.md
+++ README.md
@@ -1,49 +1,12 @@
 # Redstone integration
 
+## What remains to be done?
 
-## Install
+Implement the AWS KMS integration into the indicated spots in [index.ts](index.ts).
+
+## Install dependencies and run
 
 ```
 npm install
+npm start
 ```
-
-## Dependencies
-
-Streamr Docker environment: see https://github.com/streamr-dev/streamr-docker-dev
-
-CLI-tools: `npm install -g @streamr/cli-tools`
-
-
-## Run
-
-### 1. Start Streamr Docker environment
-
-```
-streamr-docker-dev start
-```
-
-### 2. Create a test stream and grant permissions
-
-```
-streamr stream create /test --dev --private-key=0x0000000000000000000000000000000000000000000000000000000000000001
-```
-
-```
-streamr stream grant-permission /test public publish subscribe --dev --private-key=0x0000000000000000000000000000000000000000000000000000000000000001
-```
-
-### 3. Start application
-
-```
-npm run start
-```
-
-The application starts to publish messages.
-
-### 4. Subscribe to the test stream
-
-```
-streamr stream subscribe 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf/test --dev
-```
-
-CLI-tools receives the messages and prints the payloads to the console.
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,34 @@
-import { Wallet } from 'ethers'
-import fetch from 'node-fetch'
 import { createBroker } from 'streamr-broker'
-import { ConfigTest } from 'streamr-client'
 import { CustomProvider } from './customProvider'
 import { log } from './log'
-import { signHashed } from './signingUtils'
-
-const STREAM_ID = '0x7e5f4552091a69125d5dfcb7b8c2659029395bdf/test'
-const PRIVATE_KEY = '0x0000000000000000000000000000000000000000000000000000000000000001'
-const HTTP_PLUGIN_PORT = 7171
 
 class RedstoneProvider extends CustomProvider {
     async getAddress(): Promise<string> {
-        return new Wallet(PRIVATE_KEY).address
+		// TODO: your KMS implementation here to retrieve the Ethereum address associated with private key
+        return ...
     }
 
     async signHash(msgHash: Buffer): Promise<string> {
-        return signHashed(msgHash, PRIVATE_KEY)
+		// TODO: your AWS KMS implementation here to sign with a private key
+        return ...
     }
-}
-
-const publishMessage = async () => {
-    const response = await fetch(`http://localhost:${HTTP_PLUGIN_PORT}/streams/${encodeURIComponent(STREAM_ID)}`, {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({
-            foo: new Date().toISOString()
-        })
-    })
-    log(`Published message to ${STREAM_ID}, response status: ${response.status}`)
 }
 
 const main = async () => {
     const broker = await createBroker({
         client: {
-            ...ConfigTest,
             auth: {
                 ethereum: new RedstoneProvider()
             }
         },
         plugins: {
-            http: {
-                port: HTTP_PLUGIN_PORT
-            }
+			// Add whatever interface plugins you want to use
+			websocket: {
+				port: 7170,
+			}
         }
     })
     await broker.start()
-
-    setInterval(async () => {
-        publishMessage()
-    }, 1000)
 }
 
 main()
